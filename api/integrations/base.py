8b927abd2d3df9a5f3115fb4e25fcdad4fbb8d68
Youssef Ismail <yismail@ualberta.ca>
2024-08-15 12:00:09 -0400

    remove headless

--- api/integrations/base.py
+++ api/integrations/base.py
@@ -2,9 +2,9 @@ import os
 from abc import ABC, abstractmethod
 
 from dotenv import load_dotenv
+from selenium import webdriver
 
 from api.interfaces import ListingRequest, ListingResult
-from selenium import webdriver
 
 load_dotenv()
 
@@ -20,7 +20,7 @@ class Integration(ABC):
     def getDriver(self) -> webdriver.Chrome | webdriver.Remote:
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-notifications")
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--window-size=1920,1080")
 
         if os.getenv("IN_DOCKER_CONTAINER", False):
