5ac886c9e6e8b0a3de84a7b39c7a8a69016491a5
Youssef Ismail <yismail@ualberta.ca>
2024-08-15 17:08:57 -0400

    try 921312

--- api/integrations/base.py
+++ api/integrations/base.py
@@ -21,6 +21,7 @@ class Integration(ABC):
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-notifications")
         options.add_argument("--no-sandbox")
+        options.add_argument("--remote-debugging-port=9222")
         # options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
